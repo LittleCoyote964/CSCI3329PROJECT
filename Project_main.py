ca9e3f7c8b708add04992996c3adc1798035ff41
LittleCoyote964 <nickdeleon16@gmail.com>
2025-03-17 13:51:54 -0500

    added buttons for the actions

--- Project_main.py
+++ Project_main.py
@@ -2,8 +2,14 @@ import tkinter as tk
 import tkinter.font as tkFont
 
 #will have to add action from when the customer signs in
-def handle_click():
-    print("Test, still need to add action.")
+def handle_login():
+    print("Test, this is the login button.")
+
+def handle_forgot():
+    print("Test, this is for forgot password.")
+
+def handle_newuser():
+    print("Test, this is to create a new user.")
 
 w = tk.Tk() #init Tkinter framework
 w.title("Bank Account Login")
@@ -14,11 +20,17 @@ w.geometry("600x600")
 fontStyle = tkFont.Font(family="Times New Roman", size=20)
 
 #header label
-headerLabel = tk.Label(text="(Bank Name)", font=("Time New Romman", 30))
+headerLabel = tk.Label(text="(Bank Name)", #change name whenever
+                       font=("Times New Romman", 30), 
+                       bg="maroon",
+                       fg="white")
 headerLabel.place(x=175, y = 50)
 
 #label to have the user enter their username
-userLabel = tk.Label(text="Username: ", font=fontStyle)
+userLabel = tk.Label(text="Username: ", 
+                     font=fontStyle,
+                     bg="maroon",
+                     fg="white")
 userLabel.place(x = 100, y = 150)
 
 #user entry
@@ -26,14 +38,43 @@ userEntry = tk.Entry(fg="Maroon", width=10, font=("Times New Roman", 20))
 userEntry.place(x=340,y=150)
 
 #label to have the user enter their password
-passLabel = tk.Label(text="Password:", font=fontStyle)
+passLabel = tk.Label(text="Password:",
+                      font=fontStyle,
+                      bg="maroon",
+                      fg="white")
 passLabel.place(x=100,y=250)
 
 #user entry for their password
-passEntry = tk.Entry(fg="Maroon", width=10, font=("Times New Roman", 20))
+passEntry = tk.Entry(fg="Maroon", 
+                     width=10, 
+                     font=("Times New Roman", 20),
+                     show="*")#Hides the user input
 passEntry.place(x=340,y=250)
 
 #log in button
-logButton = tk.Button(text="Login", width =23, height = 2, bg="white", fg = "black", command=handle_click)
+logButton = tk.Button(text="Login", 
+                      width =23, 
+                      height = 2, 
+                      bg="gray", 
+                      fg = "black", 
+                      command=handle_login)
 logButton.place(x=200, y = 350)
+
+#forgot password button
+forpassButt = tk.Button(text="Forgot Password?",
+                        width=23, 
+                        height=2, 
+                        bg="gray",
+                        fg="black",
+                        command = handle_forgot)
+forpassButt.place(x=200, y=400)
+
+#create new user
+newButton = tk.Button(text="Create new user",
+                      width = 23,
+                      height = 2,
+                      bg = "gray",
+                      fg = "black",
+                      command = handle_newuser)
+newButton.place(x = 200,y=450)
 w.mainloop()
