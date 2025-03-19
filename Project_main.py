06caa701520d0869a792e45eedd50bc2f32e9a78
GabrielDeCarvalh0 <gabeduke713@gmail.com>
2025-03-19 16:34:39 -0500

    Updated forgot password functionality

--- Project_main.py
+++ Project_main.py
@@ -11,16 +11,28 @@ def handle_forgot():
 def handle_newuser():
     print("Test, this is to create a new user.")
 
-w = tk.Tk() #init Tkinter framework
+w = tk.Tk()
 w.title("Bank Account Login")
-w.configure(bg = "Maroon")
-w.geometry("600x600")
+w.configure(bg="Maroon")
+
+win_width = 600
+win_height = 600
+w.geometry(f"{win_width}x{win_height}")
+
+screen_width = w.winfo_screenwidth()
+screen_height = w.winfo_screenheight()
+
+x_position = (screen_width // 2) - (win_width // 2)
+y_position = (screen_height // 2) - (win_height // 2)
+
+# Set the position
+w.geometry(f"{win_width}x{win_height}+{x_position}+{y_position}")
 
 #font style / we can change the font 
 fontStyle = tkFont.Font(family="Times New Roman", size=20)
 
 #header label
-headerLabel = tk.Label(text="(Bank Name)", #change name whenever
+headerLabel = tk.Label(text="Bank of Project", #change name whenever
                        font=("Times New Romman", 30), 
                        bg="maroon",
                        fg="white")
@@ -34,7 +46,7 @@ userLabel = tk.Label(text="Username: ",
 userLabel.place(x = 100, y = 150)
 
 #user entry
-userEntry = tk.Entry(fg="Maroon", width=10, font=("Times New Roman", 20))
+userEntry = tk.Entry(fg="White", width=10, font=("Times New Roman", 20))
 userEntry.place(x=340,y=150)
 
 #label to have the user enter their password
@@ -45,7 +57,7 @@ passLabel = tk.Label(text="Password:",
 passLabel.place(x=100,y=250)
 
 #user entry for their password
-passEntry = tk.Entry(fg="Maroon", 
+passEntry = tk.Entry(fg="White", 
                      width=10, 
                      font=("Times New Roman", 20),
                      show="*")#Hides the user input
