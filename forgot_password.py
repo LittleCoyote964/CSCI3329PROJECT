3e25e21c32f8bdc8dae200ea92f2d1fbe84ac703
Greenspore272 <adriangonzalez9801@gmail.com>
2025-05-02 19:17:33 -0500

    Gui changes to create and forgot

--- forgot_password.py
+++ forgot_password.py
@@ -6,24 +6,31 @@ from user_manager import UserManager
 class ForgotPasswordDialog(BaseDialog):
     def __init__(self, parent):
         # Sets up the forgot password window
-        super().__init__(parent, "Forgot Password", bg_color="gray")
+        super().__init__(parent, "Forgot Password", bg_color="white")
         self._user_manager = UserManager()  # Load user manager to access user data
 
     def _setup_ui(self):
         # Pops out a window to ask for username
-        tk.Label(self, text="Reset Password",
-                 font=("Times New Roman", 20), bg="gray", fg="white").pack(pady=10)
+        tk.Label(self, text="Forgot Password",
+                 font=("Times New Roman", 14, "bold"), bg="white", fg="black").place(x=10,y=35)
+        # filler label
+        tk.Label(self, text="Please provide some basic information and we'll help with your Password.",
+                 font=("Times New Roman", 9), bg="white", fg="black").place(x=10,y=70)
+        # filler1 label
+        tk.Label(self, text="Forgot User Password", font=("Times New Roman", 16), bg="red",fg="white",anchor='w', padx=10).pack(fill='x')
 
-        tk.Label(self, text="Enter your username:",
-                 font=("Times New Roman", 14), bg="gray", fg="white").pack()
+        tk.Label(self, text="User ID",
+                 font=("Times New Roman", 12), bg="white", fg="black").place(x=10,y=100)
 
         self._username_entry = tk.Entry(self, width=30, font=("Times New Roman", 12))
-        self._username_entry.pack(pady=5)
+        self._username_entry.place(x=10,y=120)
 
-        submit_button = tk.Button(self, text="Next",
+        submit_button = tk.Button(self, text="Continue",
                                   command=self._verify_username,  # Calls function to verify user exists
-                                  bg="white", fg="black", width=15)
-        submit_button.pack(pady=20)
+                                  bg="dark blue", fg="white", width=15, relief="flat")
+        submit_button.place(x=10,y=180)
+        tk.Button(self, text="Cancel", command=self.destroy,
+                  bg="white", fg="dark blue", width=15, borderwidth=2,relief="flat").place(x=150,y=180)
 
     def _verify_username(self):
         # Checks if username exists in users.json
@@ -43,17 +50,23 @@ class ForgotPasswordDialog(BaseDialog):
         # Creats a new window to enter a new password
         for widget in self.winfo_children():
             widget.destroy()
-
+        # filler1 label
+        tk.Label(self, text="Forgot User Password", font=("Times New Roman", 16), bg="red",fg="white",anchor='w', padx=10).pack(fill='x')
+        tk.Label(self, text="Forgot Password",
+                 font=("Times New Roman", 14, "bold"), bg="white", fg="black").place(x=10,y=45)
         tk.Label(self, text="Enter New Password",
-                 font=("Times New Roman", 20), bg="gray", fg="white").pack(pady=10)
+                 font=("Times New Roman", 12), bg="white", fg="black").place(x=10,y=110)
+        # filler2 label
+        tk.Label(self, text="Please provide the new password and we'll help with your Password.",
+                 font=("Times New Roman", 9), bg="white", fg="black").place(x=10,y=80)
 
         self._new_password_entry = tk.Entry(self, width=30, font=("Times New Roman", 12), show="*")
-        self._new_password_entry.pack(pady=5)
+        self._new_password_entry.place(x=10,y=130)
 
         submit_button = tk.Button(self, text="Reset Password",
                                   command=self._reset_password,  # Calls function to save new password
-                                  bg="white", fg="black", width=15)
-        submit_button.pack(pady=20)
+                                  bg="dark blue", fg="white", width=15)
+        submit_button.place(x=10,y=200)
 
     def _reset_password(self):
         # Saves the new password to the user.json
--- new_user.py
+++ new_user.py
@@ -5,29 +5,40 @@ from user_manager import UserManager
 
 class NewUserDialog(BaseDialog):
     def __init__(self, parent):
-        super().__init__(parent, "Create New User", bg_color="gray")
+        super().__init__(parent, "Create New User", bg_color="white")
         self._user_manager = UserManager()
 
     def _setup_ui(self):
-        tk.Label(self, text="Create Username and Password",
-                font=("Times New Roman", 20), bg="gray", fg="white").pack(pady=10)
+         # filler1 label
+        filler1Label = tk.Label(self, text="Enroll in Online Banking", font=("Times New Roman", 16), bg="red",fg="white",anchor='w',padx=10)      # <-- little padding from the left edge
+        filler1Label.pack(fill='x', pady=(0, 0))
+        tk.Label(self, text="Let's get started",
+                font=("Times New Roman", 14, "bold"), bg="white", fg="black").place(x=10,y=30)
+
+        tk.Label(self, text="Please provide the following information to being your enrollment.",
+                font=("Times New Roman", 10), bg="white", fg="black").place(x=10,y=60)
+
 
         tk.Label(self, text="Enter username:",
-                font=("Times New Roman", 14), bg="gray", fg="white").pack()
+                font=("Times New Roman", 12), bg="white", fg="black").place(x=10,y=80)
 
         self._username_entry = tk.Entry(self, width=30, font=("Times New Roman", 12))
-        self._username_entry.pack(pady=5)
+        self._username_entry.place(x=10,y=100)
 
         tk.Label(self, text="Enter password:",
-                font=("Times New Roman", 14), bg="gray", fg="white").pack()
+                font=("Times New Roman", 12), bg="white", fg="black").place(x=10,y=140)
 
         self._password_entry = tk.Entry(self, width=30, show='*', font=("Times New Roman", 12))
-        self._password_entry.pack(pady=5)
+        self._password_entry.place(x=10,y=160)
 
         submit_button = tk.Button(self, text="Submit",
                                 command=self._submit_user,
-                                bg="white", fg="black", width=15)
-        submit_button.pack(pady=20)
+                                bg="dark blue", fg="white", width=15)
+        
+        submit_button.place(x=10,y=200)
+        tk.Button(self, text="Cancel", command=self.destroy,
+                  bg="white", fg="dark blue", width=15, borderwidth=2,relief="flat").place(x=150,y=200)
+
 
     def _submit_user(self):
         username = self._username_entry.get().strip()
